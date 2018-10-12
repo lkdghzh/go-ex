5abb156c1475e4d2a5557b7bfe9e67aebd8158d4
likelike <likelike@didichuxing.com>
2018-10-12 18:52:32 +0800

    流程控制 break label ,goto

--- control-statements.go
+++ control-statements.go
@@ -12,4 +12,34 @@ func main() {
 	}
 	//undefined: a
 	//println(a)
+
+	//break label一样
+	for i := 0; i < 2; i++ {
+		//loop2:
+		for j := 0; j < 2; j++ {
+			break //break loop2
+			println("inner")
+		}
+		println("outer")
+	}
+l:
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			println("inner") //只打印一次inner
+			break l
+		}
+		println("outer")
+	}
+
+	//goto
+t:
+	for true {
+		for j := 0; j < 2; j++ {
+			println(j)
+			if j == 1 {
+				goto t //不主张使用goto语句， 以免造成程序流程的混乱
+			}
+		}
+		println("outer")
+	}
 }
