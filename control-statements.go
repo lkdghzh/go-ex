5b4d5031e3910907e5a4808430936b3ca368809c
likelike <likelike@didichuxing.com>
2018-10-12 11:59:38 +0800

    流程控制  多赋值语句；结果

--- control-statements.go
+++ control-statements.go
@@ -2,4 +2,14 @@ package main
 
 func main() {
 
+	//if
+	//条件判断语句里面允许声明一个变量,
+	//变量的作用域只能在该条件逻辑块内，其他地方就不起作用
+	if a := 1; a == 1 {
+		println(true)
+	} else {
+		println(false)
+	}
+	//undefined: a
+	//println(a)
 }
